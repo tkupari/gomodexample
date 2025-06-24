a8ed2341c9d1fa0aef4c7faeec2f8789e0f81769
Teemu Kupari <tkupari@gmail.com>
2025-06-24 15:40:20 +0300

    v2: make Add function generic

--- adder.go
+++ adder.go
@@ -1,11 +1,12 @@
 // Package gomodexample has been created to practice go module releases
 package gomodexample
 
-// Add returns the sum of given arguments
+// Add returns the sum of given arguments.
+// Works on integers and floats
 //
 // read more from [mathisfun]
 //
 // [mathisfun]: https://mathisfun.com/numbers/addition
-func Add(x, y int) int {
+func Add[T int | float64](x, y T) T {
 	return x + y
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/tkupari/gomodexample
+module github.com/tkupari/gomodexample/v2
 
 go 1.24.4
