2c1741bfb2e926d19c5ec6bbeb08d99c0329e32f
Teemu Kupari <tkupari@gmail.com>
2025-06-24 15:31:52 +0300

    Add documentation

--- adder.go
+++ adder.go
@@ -1,5 +1,11 @@
+// Package gomodexample has been created to practice go module releases
 package gomodexample
 
+// Add returns the sum of given arguments
+//
+// read more from [mathisfun]
+//
+// [mathisfun]: https://mathisfun.com/numbers/addition
 func Add(x, y int) int {
 	return x + y
 }
